ecaf0149a6e8c8b8050215397191bdd9e39a01b3
suri <ananthasuirv@gmail.cm>
2014-03-18 18:54:46 +0530

    Prod Config

--- ribbit/urls.py
+++ ribbit/urls.py
@@ -23,3 +23,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += patterns('django.contrib.staticfiles.views',
+        url(r'^static/(?P<path>.*)$', 'serve'),
+    )
